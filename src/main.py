b7214f558abbfafb2928dffd85a1a6fbb8ed2a15
guberger <guillaume.berger@colorado.edu>
2024-11-27 10:08:24 +0100

    Finished

--- src/main.py
+++ src/main.py
@@ -4,10 +4,12 @@ from plotting import Window
 
 population = [1, 2, 3, 4, 5]
 weights_list = [
-    [1, 2, 3, 2, 0],
-    [0, 0, 0, 1, 0],
-    [0, 1, 0, 0, 0],
+    [5, 1, 3, 2, 1],
+    [1, 1, 2, 5, 5],
+    [1, 1, 5, 1, 1],
+    [1, 3, 1, 3, 1],
+    [1, 2, 3, 2, 1],
 ]
-w = Window(population, weights_list, 1)
+w = Window(population, weights_list)
 
 plt.show()
